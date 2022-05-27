592fdfa400bd3eb6e9be856b12078c44a29cdb06
Rafael Paladini <56520474+PaMeirelles@users.noreply.github.com>
2022-05-27 17:10:50 +0000

    Comentários na main

--- main.c
+++ main.c
@@ -8,24 +8,36 @@
 
 int main(void)
 {
+  // Abrimos o arquivo a ser lido e criamos um para receber a versão compactada
   FILE * texto = fopen("poema_certo.txt", "r");
   FILE * compactado = fopen("compactado.dat", "wb");
+
+  // Lemos o arquivo fonte e extraímos a string correspondente
+  char * mensagem = le_arquivo(texto);
+
+  // Variáveis que guardarão a árvore e o tamanho do arquivo compactado. Em usos práticos, seria necessário enviar ambos para quem fosse descompactar o arquivo
   long size;
-  char * teste = le_arquivo(texto);
+  s_node * arvore = arvore_do_zero(mensagem);
 
-  s_node * arvore = arvore_do_zero(teste);
-  compacta(teste, compactado, arvore, &size);
+  // Compactamos o arquivo
+  compacta(mensagem, compactado, arvore, &size);
 
+  // Fechamos os arquivos
   fclose(texto);
   fclose(compactado);
 
+  // Importante liberar a memória ocupada pela string quando ela não for mais necessária
+  free(mensagem);
+  // --------------------------------------
+  // Agora,para descompactar, abrimos os arquivos relevantes
   FILE * comp = fopen("compactado.dat", "rb");
   FILE * txt = fopen("novo.txt", "w");
+  // Descompactamos
   descompacta(comp, txt, arvore, size);
-  
+  // Fechamos os arquivos
   fclose(comp);
   fclose(txt);
-  free(teste);
+  // Importante liberar a memória alocada pela árvore quando ela não for mais necessária
   libera_arvore(arvore);
 
   return 0;
